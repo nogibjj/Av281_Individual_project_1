b8e054818837c52598e09f5bfe9fac4f0059c211
adlerviton <143657881+adlerviton@users.noreply.github.com>
2023-09-19 22:45:29 -0400

    Update test_lib.py

--- test_lib.py
+++ test_lib.py
@@ -23,17 +23,16 @@ def test_PDdescribe():
     assert result.shape == (8, 4), "Descriptive statistics do not match expected size"
 
 
-# Sample DataFrame for testing
-data = {
-    "Age": [25, 30, 35, 40, 45, 30],
-    "Salary": [50000, 60000, 75000, 80000, 90000, 60000],
-    "Education": ["Bachelor", "Master", "Bachelor", "PhD", "Master", "Master"],
-}
-df = pd.DataFrame(data)
 
 
-def test_unique_values(df):
+def test_unique_values():
     # Test the unique_values function
+    data = {
+        "Age": [25, 30, 35, 40, 45, 30],
+        "Salary": [50000, 60000, 75000, 80000, 90000, 60000],
+        "Education": ["Bachelor", "Master", "Bachelor", "PhD", "Master", "Master"],
+    }
+    df = pd.DataFrame(data)
     unique_education = unique_values(df, "Education")
     assert (unique_education == np.array([
         "Bachelor", "Master", "PhD"
@@ -42,6 +41,12 @@ def test_unique_values(df):
 
 def test_value_counts(df):
     # Test the value_counts function
+    data = {
+        "Age": [25, 30, 35, 40, 45, 30],
+        "Salary": [50000, 60000, 75000, 80000, 90000, 60000],
+        "Education": ["Bachelor", "Master", "Bachelor", "PhD", "Master", "Master"],
+    }
+    df = pd.DataFrame(data)
     value_counts_education = value_counts(df, "Education")
     assert (
         value_counts_education["Master"] == 3
@@ -49,8 +54,8 @@ def test_value_counts(df):
 
 
 test_PDdescribe()
-test_unique_values(df)
-test_value_counts(df)
+test_unique_values()
+test_value_counts()
 
 # run data visualization code
 testing_data = pd.read_csv("nba.csv")
