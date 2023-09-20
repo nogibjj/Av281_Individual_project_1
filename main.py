edfe649f093bbaf72dff4cd7b757be6c5b67a0d2
Adler Viton <adler.viton@duke.edu>
2023-09-20 16:16:11 -0400

    update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from lib import (
     PDdescribe,
     unique_values,
     value_counts,
+    plot_histogram,
+    plot_boxplot,
+    plot_correlation_heatmap,
+    plot_countplot,
 )
 import pandas as pd
 
@@ -18,3 +22,12 @@ print("")
 print("counts of cylinders values:")
 print(value_counts(testing_data, "cylinders"))
 print("")
+
+plot_countplot(testing_data, "cylinders")
+plot_histogram(testing_data, "mpg")
+plot_boxplot(testing_data, "mpg")
+
+test_data = testing_data.drop("name", axis=1)
+print("The correlation beetween variables is visualized below")
+
+plot_correlation_heatmap(test_data)
