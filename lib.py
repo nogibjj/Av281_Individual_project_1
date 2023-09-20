aa8682d7c850c17441803516cc9e8ce100842a02
Adler Viton <adler.viton@duke.edu>
2023-09-19 21:02:41 -0400

    worked on notebook file

--- lib.py
+++ lib.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 # Function to get descriptive statistics of a data set in csv form
 def PDdescribe(filename):
     """
@@ -11,11 +12,13 @@ def PDdescribe(filename):
     df = pd.read_csv(filename)
     return df.describe()
 
+
 def unique_values(df, column):
     """
     Display unique values in a categorical column.
     """
     return df[column].unique()
+
 
 # Function to display value counts for a categorical column
 def value_counts(df, column):
@@ -24,6 +27,7 @@ def value_counts(df, column):
     """
     return df[column].value_counts()
 
+
 # Function to plot a histogram of a numeric column
 def plot_histogram(df, column):
     """
@@ -31,10 +35,11 @@ def plot_histogram(df, column):
     """
     plt.figure(figsize=(8, 6))
     sns.histplot(df[column], kde=True)
-    plt.title(f'Histogram of {column}')
+    plt.title(f"Histogram of {column}")
     plt.xlabel(column)
-    plt.ylabel('Frequency')
+    plt.ylabel("Frequency")
     plt.show()
+
 
 # Function to plot a boxplot of a numeric column
 def plot_boxplot(df, column):
@@ -43,9 +48,10 @@ def plot_boxplot(df, column):
     """
     plt.figure(figsize=(8, 6))
     sns.boxplot(x=df[column])
-    plt.title(f'Boxplot of {column}')
+    plt.title(f"Boxplot of {column}")
     plt.xlabel(column)
     plt.show()
+
 
 # Function to display a correlation matrix heatmap
 def plot_correlation_heatmap(df):
@@ -54,9 +60,10 @@ def plot_correlation_heatmap(df):
     """
     correlation_matrix = df.corr()
     plt.figure(figsize=(10, 8))
-    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
-    plt.title('Correlation Matrix Heatmap')
+    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f")
+    plt.title("Correlation Matrix Heatmap")
     plt.show()
+
 
 # Function to display a count plot of a categorical column
 def plot_countplot(df, column):
@@ -65,9 +72,8 @@ def plot_countplot(df, column):
     """
     plt.figure(figsize=(8, 6))
     sns.countplot(data=df, x=column)
-    plt.title(f'Count Plot of {column}')
+    plt.title(f"Count Plot of {column}")
     plt.xlabel(column)
-    plt.ylabel('Count')
+    plt.ylabel("Count")
     plt.xticks(rotation=45)
     plt.show()
-
