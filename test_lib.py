d39b9efd6bd87c8a3b604eeeadc6bc5dda8b652e
Adler Viton <adler.viton@duke.edu>
2023-09-19 22:18:30 -0400

    fixed tests

--- test_lib.py
+++ test_lib.py
@@ -21,23 +21,31 @@ def test_PDdescribe():
     result = PDdescribe("nba.csv")
     assert result.shape == (8, 4), "Descriptive statistics do not match expected size"
 
+
 # Sample DataFrame for testing
 data = {
-    'Age': [25, 30, 35, 40, 45, 30],
-    'Salary': [50000, 60000, 75000, 80000, 90000, 60000],
-    'Education': ['Bachelor', 'Master', 'Bachelor', 'PhD', 'Master', 'Master']
+    "Age": [25, 30, 35, 40, 45, 30],
+    "Salary": [50000, 60000, 75000, 80000, 90000, 60000],
+    "Education": ["Bachelor", "Master", "Bachelor", "PhD", "Master", "Master"],
 }
 df = pd.DataFrame(data)
 
+
 def test_unique_values():
     # Test the unique_values function
-    unique_education = unique_values(df, 'Education')
-    assert len(unique_education) == 3, "Unique values do not match expected values"
+    unique_education = unique_values(df, "Education")
+    assert unique_education == [
+        "Bachelor" "Master" "PhD"
+    ], "Unique values do not match expected values"
+
 
 def test_value_counts():
     # Test the value_counts function
-    value_counts_education = value_counts(df, 'Education')
-    assert len(value_counts_education) == 3, "Value counts do not match expected values"
+    value_counts_education = value_counts(df, "Education")
+    assert (
+        value_counts_education["Master"] == 3
+    ), "Value counts do not match expected values"
+
 
 test_PDdescribe()
 test_unique_values()
@@ -50,17 +58,16 @@ plot_countplot(testing_data, "Position")
 plot_histogram(testing_data, "Weight")
 plot_boxplot(testing_data, "Age")
 
-#test correlation heatmap
+# test correlation heatmap
 data = {
-    'Age': [25, 30, 35, 40, 45],
-    'Salary': [50000, 60000, 75000, 80000, 90000],
-    'Experience': [2, 5, 8, 10, 12]
+    "Age": [25, 30, 35, 40, 45],
+    "Salary": [50000, 60000, 75000, 80000, 90000],
+    "Experience": [2, 5, 8, 10, 12],
 }
 df = pd.DataFrame(data)
 
 # Let's add a highly correlated column for demonstration purposes
-df['Bonus'] = df['Salary'] * 0.1  # Assume Bonus is 10% of Salary
+df["Bonus"] = df["Salary"] * 0.1  # Assume Bonus is 10% of Salary
 
 # Plot the correlation matrix heatmap
 plot_correlation_heatmap(df)
-
